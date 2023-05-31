c64a6db067a65f3ad28aee74c65b70c60e30ebee
민주 정 <minju9711@naver.com>
2023-05-31 19:23:29 +0900

    +버튼 안보이게 하는 거만 남음 - 버튼은 뭐 +처럼 하면 되겠지

--- todo_list.js
+++ todo_list.js
@@ -1,15 +1,17 @@
-//let number=1;
-init();
+let number=1;
 
 function init() {
     let myArray = Array.from({ length: 7 }, (_, index) => index + 1);
-
     myArray.forEach(function (item) {
-        push_plus(number);
+       push_plus();
     });
 }
 
-function push_plus(number) {
+window.addEventListener('load',init());
+//무조건 이 함수 먼저 실행되게 해주는 것 -> 제일 최상위객체 window
+
+function push_plus() {
+    let number=0;
     const table = document.getElementById('table');
     const tbody = document.getElementById('tbody');
 
@@ -31,8 +33,9 @@ function push_plus(number) {
     input_2.textContent = "";
     input_2.setAttribute('id', `${number}`);
 
-    number++;
     console.log(number);
+    number++;
+ 
 
     plus_btn.textContent = " + ";
     span.appendChild(plus_btn);
@@ -47,12 +50,18 @@ function push_plus(number) {
     check_btn.textContent = "check";
     tr.appendChild(check_btn);
 
-    plus_btn.addEventListener('click', push_plus(number));
+    
+    plus_btn.addEventListener('click', () => { push_plus()}); 
+    //여기서 화살표 함수를 쓰지 않고 밑과 같은 방식으로 호출하게 되면 무한재귀호출에 걸리게 됨.
     check_btn.addEventListener('click', push_check);
-
+    
 }
 
 function push_check() {
-
+    console.log('push check');
 }
+
+const plus = document.getElementById('plus_btn');
+console.log(plus);
+plus.addEventListener('click', push_plus());
 

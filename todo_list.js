be14cfe4e2c6c4a2d0439683f22f9343779022f2
민주 정 <minju9711@naver.com>
2023-06-04 02:14:36 +0900

    input의 비율 맞춰주기

--- todo_list.js
+++ todo_list.js
@@ -11,12 +11,13 @@ window.addEventListener('load', init());
 //무조건 이 함수 먼저 실행되게 해주는 것 -> 제일 최상위객체 window
 
 function push_plus() {
-    if(number!==1){
-        const fade_delete = document.getElementById(`delete_btn${number-1}`);
-        const fade_plus = document.getElementById(`plus_btn${number-1}`);
+    if (number !== 1) {
+        const fade_delete = document.getElementById(`delete_btn${number - 1}`);
+        const fade_plus = document.getElementById(`plus_btn${number - 1}`);
         fade_delete.style.display = 'none';
         fade_plus.style.display = 'none';
     }
+
     const tbody = document.getElementById('tbody');
 
     const tr = document.createElement('tr');
@@ -39,6 +40,15 @@ function push_plus() {
     input_2.type = 'text';
     input_2.textContent = "";
 
+    /*
+
+    // input_1의 스타일 설정
+    input_1.style.width = "25%";
+
+    // input_2의 스타일 설정
+    input_2.style.width = "75%";
+    */
+
     number++;
     plus_btn.textContent = " + ";
     check_btn.textContent = "no";
@@ -46,12 +56,14 @@ function push_plus() {
 
     btn_td.appendChild(delete_btn); // 버튼을 td 요소에 추가
     btn_td.appendChild(plus_btn);
-    
+
     tr.appendChild(btn_td); // td 요소를 tr 요소에 추가
     tr.appendChild(input_1);
     tr.appendChild(input_2);
     tr.appendChild(check_btn);
-    tbody.appendChild(tr);
+
+    // tbody의 두 번째 자식으로 새로운 행(tr) 추가
+    tbody.insertBefore(tr, tbody.children[number - 1]);
 
     plus_btn.addEventListener('click', push_plus); // 노션 문제점 1 참고
     delete_btn.addEventListener('click', delete_row);
@@ -67,12 +79,12 @@ function push_check(event) {
 }
 
 function delete_row() {
-    const delete_target = document.getElementById(`${number-1}`);
+    const delete_target = document.getElementById(`${number - 1}`);
     delete_target.remove();
     number--;
 
-    const show_delete = document.getElementById(`delete_btn${number-1}`);
-    const show_plus = document.getElementById(`plus_btn${number-1}`);
+    const show_delete = document.getElementById(`delete_btn${number - 1}`);
+    const show_plus = document.getElementById(`plus_btn${number - 1}`);
     show_delete.style.display = 'inline';
     show_plus.style.display = 'inline';
 
